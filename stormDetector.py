092f348f3df497b306f56bec6ee50df9e328c76a
Srikanth <sriksrin@iu.edu>
2016-09-18 22:38:32 -0400

    updating Storm Detector Branch
    updating Storm Detector Branch

--- stormDetector.py
+++ stormDetector.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify
-from flask import request
-from flask import make_response
-import random
-import urllib.request as read
+import urllib.request
+import urllib.error
+from bs4 import BeautifulSoup
+import json
+
 
 app = Flask(__name__)
 
@@ -12,25 +13,35 @@ app = Flask(__name__)
 @app.route('/https://noaa-nexrad-level2.s3.amazonaws.com/<yy>/<mm>/<dd>/<stationId>/<filename>', methods=['GET'])
 def stormDetector(yy=None, mm=None, dd=None, stationId=None, filename=None):
 
-   
-    f = open('test.txt','w')
-    f.write(yy)
-    f.write(mm)
-    f.write(dd)
-    f.write(stationId)
-    f.write(filename)
-    f.close()
 
-    list = [
-        {'param': 'foo', 'val': stationId},
-        {'param': 'bar', 'val': filename}
-    ]
-        #flag = random.getrandbits(1)
+    baseUrl = 'https://noaa-nexrad-level2.s3.amazonaws.com/'
+    kmlUrl =  baseUrl + yy + '/' + mm + '/' + dd + '/' + stationId + '/' + filename
+
+
+    responseCode = 200
+    response = urllib.request.urlopen(kmlUrl)
+    data = response.read()
+    text = BeautifulSoup(data).encode('utf-8')
+
+    kmlStream = ""
+
+    with open('KML_Samples.kml', 'r') as f:
+        kmlStream += str(f.readlines())
+
+
+    list = {'param': 'byteStream',   'val': kmlStream}
+
+    json_str = json.dumps(list)
+    json_data = json.loads(json_str)
+
+    with open('data.json', 'w') as f:
+        json.dump(json_data, f)
+
     return jsonify(results=list)
 
 if __name__ == '__main__':
     app.run(
         host="127.0.0.1",
         port=int(5000),
-        #debug=True
+        debug=True
     )
