6fef36e8fcb2c7c8277f1da97352707d9062334e
Srikanth <sriksrin@iu.edu>
2016-09-20 11:34:15 -0400

    integrated stormdetector with api gateway

--- stormDetector.py
+++ stormDetector.py
@@ -1,12 +1,41 @@
-from flask import Flask, jsonify
-import urllib.request
-import urllib.error
-from bs4 import BeautifulSoup
+import datetime
 import json
+import urllib.error
+import urllib.request
+
+from bs4 import BeautifulSoup
+from flask_sqlalchemy import SQLAlchemy
+
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
+app.debug = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dbadmin:teamauroradbadmin@teamaurora-db.cfofssvi9hbo.us-west-2.rds.amazonaws.com/sg_teamaurora_db'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
 
+class Service_Requests_Log(db.Model):
+    #RequestID, TimeStamp, Input, Output , Service
+    __tablename__ = 'Service_Requests_Logger'
+    id = db.Column(db.Integer, primary_key=True)
+    request_id = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime)
+    input = db.Column(db.String(80))
+    output = db.Column(db.String(80))
+    service_name = db.Column(db.String(40))
+
+    db.PrimaryKeyConstraint('id')
+
+    def __init__(self,request_id, timestamp, Input, Output, Service):
+        self.timestamp = timestamp
+        self.request_id = request_id
+        self.input = Input
+        self.output = Output
+        self.service_name = Service
+
+    def __repr__(self):
+        return self.request_id
 
 @app.route('/') 
 @app.route('/noaa-nexrad-level2.s3.amazonaws.com/<yy>/<mm>/<dd>/<stationId>/<filename>', methods=['GET'])
@@ -29,13 +58,17 @@ def stormDetector(yy=None, mm=None, dd=None, stationId=None, filename=None):
         kmlStream += str(f.readlines())
 
 
-    list = {'param': 'byteStream',   'val': kmlStream}
+    list = {'param': 'byteStream', 'val': kmlStream}
 
     json_str = json.dumps(list)
     json_data = json.loads(json_str)
 
     with open('data.json', 'w') as f:
         json.dump(json_data, f)
+
+    requestLog = Service_Requests_Log(1,datetime.datetime.now(),kmlUrl,"data.json","StormDetction")
+    db.session.add(requestLog)
+    db.session.commit()
 
     return jsonify(results=list)
 
