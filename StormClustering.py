f3f32f931db6e80974aaf3795821963e63a58fed
Srikanth <sriksrin@iu.edu>
2016-09-20 11:29:36 -0400

    integrated with api gateway

--- StormClustering.py
+++ StormClustering.py
@@ -1,30 +1,63 @@
-from flask import Flask, render_template, request, jsonify
-import json
+import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify, request,json
+
 
 app = Flask(__name__)
+app.debug = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dbadmin:teamauroradbadmin@teamaurora-db.cfofssvi9hbo.us-west-2.rds.amazonaws.com/sg_teamaurora_db'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class Service_Requests_Log(db.Model):
+    #RequestID, TimeStamp, Input, Output , Service
+    __tablename__ = 'Service_Requests_Logger'
+    id = db.Column(db.Integer, primary_key=True)
+    request_id = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime)
+    input = db.Column(db.String(80))
+    output = db.Column(db.String(80))
+    service_name = db.Column(db.String(40))
+
+    db.PrimaryKeyConstraint('id')
+
+    def __init__(self,request_id, timestamp, Input, Output, Service):
+        self.timestamp = timestamp
+        self.request_id = request_id
+        self.input = Input
+        self.output = Output
+        self.service_name = Service
+
+    def __repr__(self):
+        return self.request_id
 
 @app.route('/StormClustering', methods=['GET', 'POST'])
-def stormClustering(req=None):
+def stormClustering():
 
+    data = request.json
 
-    with open('data.json', 'r') as f:
-        data = json.load(f)
+    kmlStream = str(data['results']['val'])
 
-    f.close()
-    kmlStream = data['val']
+    with open('test.txt' , 'w') as f:
+        f.write(str(kmlStream))
+
 
     responseCode = 200
-
     list = [
         {'param': 'responseCode', 'val': responseCode},
-        {'param': 'byteStream',   'val': kmlStream}
+        {'param': 'byteStream',   'val': "dummy clusters"}
     ]
+
+    '''
+    requestLog = Service_Requests_Log(1,datetime.datetime.now(),"data.json","clustering","StormClustering")
+    db.session.add(requestLog)
+    db.session.commit()'''
 
     return jsonify(results=list)
 
 if __name__ == '__main__':
     app.run(
         host="127.0.0.1",
-        port=int(5000),
+        port=int(5050),
         #debug=True
     )
