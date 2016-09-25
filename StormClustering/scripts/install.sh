c20ca87e457f9002b66024a16ba9dbcbfa2eacdf
Srikanth <sriksrin@iu.edu>
2016-09-25 03:24:27 -0400

    Changed structure of storm clustering
    Changed structure of storm clustering

---
+++ StormClustering/scripts/install.sh
@@ -0,0 +1,14 @@
+echo 'starting installation process' >> /var/log/sga-teamaurora-StormClustering-install.log
+cd '/home/ec2-user/StormClustering'
+
+echo 'Activating virtualenv for StormClustering Microservice' >> /var/log/sga-teamaurora-StormClustering-install.log
+pip install virtualenv >> /var/log/sga-teamaurora-StormClustering-install.log
+virtualenv env >> /var/log/sga-teamaurora-StormClustering-install.log
+source env/bin/activate >> /var/log/sga-teamaurora-StormClustering-install.log
+pip install Flask >> /var/log/sga-teamaurora-StormClustering-install.log
+pip install nose >> /var/log/sga-teamaurora-StormClustering-install.log
+pip install BeautifulSoup4 >> /var/log/sga-teamaurora-StormClustering-install.log
+pip install Flask-SQLAlchemy >> /var/log/sga-teamaurora-StormClustering-install.log
+#echo 'Running Flask Server' >> /var/log/sga-teamaurora-flask-install.log
+export FLASK_APP=StormClustering.py
+flask run --host=0.0.0.0 --port=65000 >> /var/log/sga-teamaurora-StormClustering-server.log 2>&1 &
