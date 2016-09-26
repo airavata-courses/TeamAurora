bae52c9d59aba7f47e87541eef483cd60febae15
pratiksanghvi <pratiksanghvi.ms@gmail.com>
2016-09-26 17:20:10 -0400

    Updated.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,5 @@
 echo 'Installing API Gateway' 
-cd '/home/ec2-user/api_gateway'
-cp api_gateway/apigateway.war /usr/share/tomcat8/webapps/apigateway.war
+cp /home/ec2-user/api_gateway/apigateway.war /usr/share/tomcat8/webapps/apigateway.war
+echo 'Starting Tomcat service. This may take some time.'
 sudo service tomcat8 start
+sleep 30
