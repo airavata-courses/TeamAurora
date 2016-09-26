cf54b312df979a6c82b4912d3f6306e681f602f3
pratiksanghvi <pratiksanghvi.ms@gmail.com>
2016-09-26 04:29:30 -0400

    Creating installation scripts.

---
+++ scripts/before_install.sh
@@ -0,0 +1,15 @@
+echo 'killing existing tomcat process if any'
+cd /usr/local/tomcat7/apache-tomcat-7.0.72
+sudo sh ./bin/shutdown.sh
+sleep 30
+echo 'Setting environment'
+export JAVA_HOME=/usr/lib/jvm/java
+echo 'check if maven is installed'
+mvn --version
+if [ "$?" -ne 0 ]; then
+    echo 'Installing Maven...'
+	sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+	sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+	sudo yum install -y apache-maven
+	mvn --version
+fi
