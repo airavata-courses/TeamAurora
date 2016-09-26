3dce535ecf7902391345ec06f257207834245e42
pratiksanghvi <pratiksanghvi.ms@gmail.com>
2016-09-26 18:09:35 -0400

    Deleted install.sh as it is corrupted.

--- scripts/install.sh
+++
@@ -1,5 +0,0 @@
-echo 'Installing API Gateway'
-cp /home/ec2-user/data_ingestor/dataingestor.war /usr/share/tomcat8/webapps/dataingestor.war
-echo 'Starting Tomcat service. This may take some time.'
-sudo service tomcat8 start
-sleep 30
