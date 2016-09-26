6d5e1765d08e4c469c0f0453e0f9f894de405db3
Kushal Sheth <kushalsheth93@gmail.com>
2016-09-26 17:29:22 -0400

    before install edit

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,10 +1,9 @@
-echo 'killing existing tomcat process if any'
-cd /usr/local/tomcat7/apache-tomcat-7.0.72
-sudo sh ./bin/shutdown.sh
+echo 'Stopping Tomcat Service. This will take some time.'
+sudo service tomcat8 stop
 sleep 30
 echo 'Setting environment'
 export JAVA_HOME=/usr/lib/jvm/java
-echo 'check if maven is installed'
+echo 'Checking if MAVEN is installed'
 mvn --version
 if [ "$?" -ne 0 ]; then
     echo 'Installing Maven...'
