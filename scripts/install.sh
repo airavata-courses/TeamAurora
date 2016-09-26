118021a96632fcaf754002985b25f5b64090f0cf
Kushal Sheth <kushalsheth93@gmail.com>
2016-09-26 17:28:48 -0400

    edit install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,5 @@
 echo 'Installing Forecast Trigger' 
-cd '/home/ec2-user/ForecastTrigger'
-sudo mvn -e clean install >> /var/log/ForecastTrigger.log
-cp reqDir/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/ >> /var/log/tomcat.log
-cd  /usr/local/tomcat7/apache-tomcat-7.0.72
-
-sudo sh ./bin/startup.sh >> /var/log/tomcat.log 2>&1 &
+cp /home/ec2-user/forecast_trigger/forecasttrigger.war /usr/share/tomcat8/webapps/forecasttrigger.war
+echo 'Starting Tomcat service. This may take some time.'
+sudo service tomcat8 start
+sleep 30
