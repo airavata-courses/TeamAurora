17b52bef7fba7d5db206c093ac0c10a9cf39b7d2
Kushal Sheth <kushalsheth93@gmail.com>
2016-09-26 19:23:05 -0400

    install.sh edit

--- StormClustering/scripts/install.sh
+++ StormClustering/scripts/install.sh
@@ -1,9 +1,10 @@
 echo 'starting installation process' >> /var/log/sga-teamaurora-StormClustering-install.log
+cd ./home/ec2-user
 mkdir -p python_libs
 cd python_libs
 mkdir -p stormclustering
-cd '/home/ec2-user/StormClustering'
-mv ./home/ec2-user/StormClustering ./python_libs/stormclustering/StormClustering
+cd ./home/ec2-user/StormClustering
+
 
 echo 'Activating virtualenv for StormClustering Microservice' >> /var/log/sga-teamaurora-StormClustering-install.log
 pip install virtualenv >> /var/log/sga-teamaurora-StormClustering-install.log
@@ -16,3 +17,4 @@ pip install Flask-SQLAlchemy >> /var/log/sga-teamaurora-StormClustering-install.
 
 export FLASK_APP=StormClustering.py
 flask run --host=0.0.0.0 --port=65000 >> /var/log/sga-teamaurora-StormClustering-server.log 2>&1 &
+mv ./home/ec2-user/StormClustering ./python_libs/stormclustering/StormClustering
