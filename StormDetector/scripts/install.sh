cd6e243dd2ee37afb607b1af8580d6e29e51ebe2
SrikanthHolavanahalli <sriksrin@iu.edu>
2016-09-26 21:45:44 -0400

    updated directory for run

--- StormDetector/scripts/install.sh
+++ StormDetector/scripts/install.sh
@@ -9,16 +9,17 @@ cp -R /home/ec2-user/StormDetector ./stormdetector/StormDetector
 
 cd ./stormdetector
 
-echo 'Activating virtualenv for StormDetection Microservice' >> /var/log/sga-teamaurora-StormDetection-install.log
-pip install -r /home/ec2-user/python_libs/stormdetector/StormDetector/requirements.txt >>  /var/log/sga-teamaurora-StormDetection-install.log
-pip install virtualenv >> /var/log/sga-teamaurora-StormDetection-install.log
-virtualenv env >> /var/log/sga-teamaurora-StormDetection-install.log
-source env/bin/activate >> /var/log/sga-teamaurora-StormDetection-install.log
-pip install Flask >> /var/log/sga-teamaurora-StormDetection-install.log
-pip install nose >> /var/log/sga-teamaurora-StormDetection-install.log
-pip install BeautifulSoup4 >> /var/log/sga-teamaurora-StormDetection-install.log
-pip install bs4 >> /var/log/sga-teamaurora-StormDetection-install.log
-pip install Flask-SQLAlchemy >> /var/log/sga-teamaurora-StormDetection-install.log
+echo 'Activating virtualenv for StormDetection Microservice' 
+pip install -r /home/ec2-user/python_libs/stormdetector/StormDetector/requirements.txt 
+pip install virtualenv
+cd StormDetector
+virtualenv env
+source env/bin/activate
+pip install Flask
+pip install nose
+pip install BeautifulSoup4
+pip install bs4
+pip install Flask-SQLAlchemy
 #echo 'Running Flask Server' >> /var/log/sga-teamaurora-flask-install.log
 export FLASK_APP=StormDetection.py
 flask run --host=0.0.0.0 --port=5000 >> /var/log/sga-teamaurora-StormDetection-server.log 2>&1 &
