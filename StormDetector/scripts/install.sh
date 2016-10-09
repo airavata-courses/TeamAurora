3a0d0a3c96fceb36b762718e58aef88a71f5769b
SrikanthHolavanahalli <sriksrin@iu.edu>
2016-10-09 17:20:56 -0400

    checking nosetests for python

--- StormDetector/scripts/install.sh
+++ StormDetector/scripts/install.sh
@@ -29,4 +29,5 @@ echo 'Running Flask Server' >> /var/log/sga-teamaurora-flask-install1.log
 #flask run --host=0.0.0.0 --port=5000 >> /var/log/sga-teamaurora-StormDetection-server.log 2>&1 &
 
 echo 'starting Storm Detection'
+pwd
 python3 StormDetector/StormDetector.py >> /var/log/sga-teamaurora-StormDetection-server1.log 2>&1 &
