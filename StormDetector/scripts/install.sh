33f6cec46e98aa24ce3feba238f829c4b287449f
SrikanthHolavanahalli <sriksrin@iu.edu>
2016-10-09 17:13:18 -0400

    checking nosetests for python 1.21

--- StormDetector/scripts/install.sh
+++ StormDetector/scripts/install.sh
@@ -24,9 +24,9 @@ pip-3.4 install nose
 pip-3.4 install BeautifulSoup4
 pip-3.4 install bs4
 pip-3.4 install Flask-SQLAlchemy
-echo 'Running Flask Server' >> /var/log/sga-teamaurora-flask-install.log
+echo 'Running Flask Server' >> /var/log/sga-teamaurora-flask-install1.log
 #export FLASK_APP=StormDetection.py
 #flask run --host=0.0.0.0 --port=5000 >> /var/log/sga-teamaurora-StormDetection-server.log 2>&1 &
 
 echo 'starting Storm Detection'
-python3 StormDetector/StormDetector.py >> /var/log/sga-teamaurora-StormDetection-server.log 2>&1 &
+python3 StormDetector/StormDetector.py >> /var/log/sga-teamaurora-StormDetection-server1.log 2>&1 &
