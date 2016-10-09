3cde91513f62de8f4c6e20864b4564c7eab43e62
SrikanthHolavanahalli <sriksrin@iu.edu>
2016-10-09 03:06:33 -0400

    checking nosetests for python

--- StormClustering/scripts/install.sh
+++ StormClustering/scripts/install.sh
@@ -5,21 +5,21 @@ cd python_libs
 mkdir -p stormclustering
 
 cp /home/ec2-user/appspec.yml ./stormclustering/appspec.yml
-cp -R /home/ec2-user/StormClustering ./stormclustering/StormClustering
+cp -R /home/ec2-user/StormClustering ./stormclustering
 
 
 cd ./stormclustering
 
 echo 'Activating virtualenv for StormClustering Microservice' >> /var/log/sga-teamaurora-StormClustering-install.log
-pip install virtualenv >> /var/log/sga-teamaurora-StormClustering-install.log
+pip-3.4 install virtualenv >> /var/log/sga-teamaurora-StormClustering-install.log
 virtualenv env >> /var/log/sga-teamaurora-StormClustering-install.log
 source env/bin/activate >> /var/log/sga-teamaurora-StormClustering-install.log
-pip install Flask >> /var/log/sga-teamaurora-StormClustering-install.log
-pip install nose >> /var/log/sga-teamaurora-StormClustering-install.log
-pip install BeautifulSoup4 >> /var/log/sga-teamaurora-StormClustering-install.log
-pip install Flask-SQLAlchemy >> /var/log/sga-teamaurora-StormClustering-install.log
-
-export FLASK_APP=StormClustering.py
-flask run --host=0.0.0.0 --port=5000 >> /var/log/sga-teamaurora-StormClustering-server.log 2>&1 &
+pip-3.4 install Flask >> /var/log/sga-teamaurora-StormClustering-install.log
+pip-3.4 install nose >> /var/log/sga-teamaurora-StormClustering-install.log
+pip-3.4 install BeautifulSoup4 >> /var/log/sga-teamaurora-StormClustering-install.log
+pip-3.4 install Flask-SQLAlchemy >> /var/log/sga-teamaurora-StormClustering-install.log
+pip-3.4 install bs4
+#export FLASK_APP=StormClustering.py
+#flask run --host=0.0.0.0 --port=5000 >> /var/log/sga-teamaurora-StormClustering-server.log 2>&1 &
 python3 StormClustering/StormClustering.py
 
