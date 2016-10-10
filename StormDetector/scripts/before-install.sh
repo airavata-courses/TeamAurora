1045a398d2dc31eb8c9aef0cd4bd25c0e92073fe
SrikanthHolavanahalli <sriksrin@iu.edu>
2016-10-09 21:05:46 -0400

    checking nosetests for python

--- StormDetector/scripts/before-install.sh
+++ StormDetector/scripts/before-install.sh
@@ -2,4 +2,4 @@ echo 'Kill All the Process' >> /var/log/flask-before.log
 ps -ef | grep python | grep -v grep | awk '{print $2}' | xargs kill >> /var/log/flask-before.log
 sleep 5
 
-chmod 777 /home/ec2-user/StormDetector/scripts/install.sh
+#chmod 777 /home/ec2-user/StormDetector/scripts/install.sh
