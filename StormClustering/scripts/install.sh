548547848f33c41e117b96995b21669eca4ae608
SrikanthHolavanahalli <sriksrin@iu.edu>
2016-10-09 23:36:31 -0400

    checking nosetests for python

--- StormClustering/scripts/install.sh
+++ StormClustering/scripts/install.sh
@@ -1,4 +1,4 @@
-echo 'starting installation process' >> /var/log/sga-teamaurora-StormClustering-install.log
+echo 'starting installation process' >> /var/log/sga-teamaurora-StormClustering1-install.log
 cd /home/ec2-user
 mkdir -p python_libs
 cd python_libs
@@ -9,7 +9,7 @@ cp -R /home/ec2-user/StormClustering ./stormclustering
 
 cd ./stormclustering
 
-echo 'Activating virtualenv for StormClustering Microservice' >> /var/log/sga-teamaurora-StormClustering-install.log
+echo 'Activating virtualenv for StormClustering Microservice' >> /var/log/sga-teamaurora-StormClustering1-install.log
 pip-3.4 install virtualenv >> /var/log/sga-teamaurora-StormClustering-install.log
 virtualenv env >> /var/log/sga-teamaurora-StormClustering-install.log
 source env/bin/activate >> /var/log/sga-teamaurora-StormClustering-install.log
@@ -21,5 +21,7 @@ pip-3.4 install bs4
 #export FLASK_APP=StormClustering.py
 #flask run --host=0.0.0.0 --port=5000 >> /var/log/sga-teamaurora-StormClustering-server.log 2>&1 &
 echo 'running StormClustering'
+pwd >> /var/log/sga-teamaurora-StormClustering-server1.log
+ls -la >> /var/log/sga-teamaurora-StormClustering-server1.log 
 python3 StormClustering/StormClustering.py
 
