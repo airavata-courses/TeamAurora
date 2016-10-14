5c5dc885afb48274348c7dbbc9832f30729f92fe
pratiksanghvi <pratiksanghvi.ms@gmail.com>
2016-10-13 21:05:16 -0400

    Moving copying artifacts code from install.sh to before_install.sh.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,4 @@
 echo '***Installing API Gateway***' 
 
-cd /home/ec2-user
-mkdir -p java_libs
-cd java_libs
-mkdir -p apigateway
-cd apigateway
-mkdir -p $(date +%m%d%Y)
-cd $(date +%m%d%Y)
-
-cp /home/ec2-user/appspec.yml .
-cp -R /home/ec2-user/api_gateway/apigateway.war .
 
 echo '***Building docker image. This may take some time.***'
