afbe57da9e7638c0df087acd498c34a54dc1b848
pratiksanghvi <pratiksanghvi.ms@gmail.com>
2016-10-13 20:26:07 -0400

    Updated the copy destination directory.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,12 @@
 echo 'Installing API Gateway' 
 
+cd /home/ec2-user
 mkdir -p java_libs
 cd java_libs
 mkdir -p apigateway
+cd apigateway
+mkdir -p $(date +%m%d%Y)
+cd $(date +%m%d%Y)
 
 cp /home/ec2-user/appspec.yml ./apigateway/appspec.yml
 cp -R /home/ec2-user/api_gateway/apigateway.war ./apigateway/apigateway.war
@@ -10,4 +14,4 @@ cp -R /home/ec2-user/api_gateway/apigateway.war ./apigateway/apigateway.war
 cp /home/ec2-user/api_gateway/apigateway.war /usr/share/tomcat8/webapps/apigateway.war
 echo 'Starting Tomcat service. This may take some time.'
 sudo service tomcat8 start
-sleep 30
+sleep 15
