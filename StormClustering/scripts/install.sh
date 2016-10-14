ecbdf739ffa2b8e58a1e2dd082161c6d285c2dd8
SrikanthHolavanahalli <sriksrin@iu.edu>
2016-10-13 21:42:04 -0400

    updated install.sh with docker commands

--- StormClustering/scripts/install.sh
+++ StormClustering/scripts/install.sh
@@ -6,22 +6,11 @@ mkdir -p stormclustering
 
 cp /home/ec2-user/appspec.yml ./stormclustering/appspec.yml
 cp -R /home/ec2-user/StormClustering ./stormclustering
+cd stormclustering/StormClustering/
 
-cd ./stormclustering
+pip3 install -r requirements.txt
 
-echo 'Activating virtualenv for StormClustering Microservice' >> /var/log/sga-teamaurora-StormClustering1-install.log
-pip-3.4 install virtualenv >> /var/log/sga-teamaurora-StormClustering-install.log
-virtualenv env >> /var/log/sga-teamaurora-StormClustering-install.log
-source env/bin/activate >> /var/log/sga-teamaurora-StormClustering-install.log
-pip-3.4 install Flask >> /var/log/sga-teamaurora-StormClustering-install.log
-pip-3.4 install nose >> /var/log/sga-teamaurora-StormClustering-install.log
-pip-3.4 install BeautifulSoup4 >> /var/log/sga-teamaurora-StormClustering-install.log
-pip-3.4 install Flask-SQLAlchemy >> /var/log/sga-teamaurora-StormClustering-install.log
-pip-3.4 install bs4
-#export FLASK_APP=StormClustering.py
-#flask run --host=0.0.0.0 --port=5000 >> /var/log/sga-teamaurora-StormClustering-server.log 2>&1 &
-echo 'running StormClustering service'
-pwd >> /var/log/sga-teamaurora-StormClustering-server1.log
-ls -la >> /var/log/sga-teamaurora-StormClustering-server1.log 
+docker build -t stormclustering .
+docker run stormclustering
 #python3 StormClustering/StormClustering.py
 
