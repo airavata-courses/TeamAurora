95bc43337b036a40a5d87a956aab67165927645e
pratiksanghvi <pratiksanghvi.ms@gmail.com>
2016-10-13 21:44:33 -0400

    Updated to build and run docker image.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,6 @@
-echo '***Installing API Gateway***' 
 
+cd /home/ec2-user/java_libs/apigateway/$(date +%m%d%Y)/api_gateway/docker/images/
 
 echo '***Building docker image. This may take some time.***'
+docker build -t apigateway .
+docker run apigateway &
