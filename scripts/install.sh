78544c2f932f7a67e88699a2aa307eeaaf41fbb2
pratiksanghvi <pratiksanghvi.ms@gmail.com>
2016-10-13 20:43:13 -0400

    Updated.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
-echo 'Installing API Gateway' 
+echo '***Installing API Gateway***' 
 
 cd /home/ec2-user
 mkdir -p java_libs
@@ -8,10 +8,7 @@ cd apigateway
 mkdir -p $(date +%m%d%Y)
 cd $(date +%m%d%Y)
 
-cp /home/ec2-user/appspec.yml ./apigateway/appspec.yml
-cp -R /home/ec2-user/api_gateway/apigateway.war ./apigateway/apigateway.war
+cp /home/ec2-user/appspec.yml .
+cp -R /home/ec2-user/api_gateway/apigateway.war .
 
-cp /home/ec2-user/api_gateway/apigateway.war /usr/share/tomcat8/webapps/apigateway.war
-echo 'Starting Tomcat service. This may take some time.'
-sudo service tomcat8 start
-sleep 15
+echo '***Building docker image. This may take some time.***'
