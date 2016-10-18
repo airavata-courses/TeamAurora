c40764d4c04bfe218b4380788d74cf5cf84ced06
SrikanthHolavanahalli <sriksrin@iu.edu>
2016-10-17 20:36:16 -0400

    updating log files

--- StormClustering/scripts/install.sh
+++ StormClustering/scripts/install.sh
@@ -1,4 +1,4 @@
-echo 'starting installation process' >> /var/log/sga-teamaurora-StormClustering1-install.log
+echo 'starting installation process' >> /var/log/SGATeamAuroraMileStone2/sga-teamaurora-StormClustering1-install.log
 cd /home/ec2-user
 mkdir -p python_libs
 cd python_libs
@@ -10,10 +10,10 @@ cd stormclustering/StormClustering/
 
 pip3 install -r requirements.txt
 
-echo "Starting Docker Build" >> /var/log/sga-teamaurora-StormClustering-installM2.log
+echo "Starting Docker Build" >> /var/log/SGATeamAuroraMileStone2/sga-teamaurora-StormClustering-installM2.log
 
-docker build -t stormclustering .  >> /var/log/sga-teamaurora-StormClustering-serverM2.log
-docker run stormclustering & >> /var/log/sga-teamaurora-StormClustering-serverM2.log
+docker build -t stormclustering .  >> /var/log/SGATeamAuroraMileStone2/sga-teamaurora-StormClustering-serverM2.log
+docker run stormclustering & >> /var/log/SGATeamAuroraMileStone2/sga-teamaurora-StormClustering-serverM2.log
 #python3 StormClustering/StormClustering.py
 
 
