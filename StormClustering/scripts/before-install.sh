4592678d5b77352d317401ad5b1c35f15aa8282e
SrikanthHolavanahalli <sriksrin@iu.edu>
2016-10-18 22:56:07 -0400

    added docker build statements

--- StormClustering/scripts/before-install.sh
+++ StormClustering/scripts/before-install.sh
@@ -1,3 +1,5 @@
 #echo 'Kill All the Process' >> /var/log/flask-before.log
 #ps -ef | grep python | grep -v grep | awk '{print $2}' | xargs kill >> /var/log/flask-before.log
 #sleep 5
+docker rm -f $(docker ps -a)
+docker build -t stormclustering .  >> /var/log/SGATeamAuroraMileStone2/sga-teamaurora-StormClustering-serverM2.log
