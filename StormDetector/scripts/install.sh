a70ceb52bc3193d286bafde481d29788bd85e049
SrikanthHolavanahalli <sriksrin@iu.edu>
2016-10-19 18:48:52 -0400

    updated pip command in install.sh

--- StormDetector/scripts/install.sh
+++ StormDetector/scripts/install.sh
@@ -15,23 +15,23 @@ cd ./stormdetector
 #sudo alternatives --set python /usr/bin/python3.4
 
 echo 'Activating virtualenv for StormDetection Microservice' 
-#pip-3.4 install -r /home/ec2-user/python_libs/stormdetector/StormDetector/requirements.txt 
-usr/local/bin/pip-3.4 install virtualenv
+pip-3 install -r /home/ec2-user/python_libs/stormdetector/StormDetector/requirements.txt 
+#usr/local/bin/pip-3.4 install virtualenv
 #cd StormDetector
-virtualenv env
-source env/bin/activate
-usr/local/bin/pip-3.4 install Flask
-usr/local/bin/pip-3.4 install nose
-usr/local/bin/pip-3.4 install BeautifulSoup4
-usr/local/bin/pip-3.4 install bs4
-usr/local/bin/pip-3.4 install Flask-SQLAlchemy
+#virtualenv env
+#source env/bin/activate
+usr/local/bin/pip-3 install Flask
+usr/local/bin/pip-3 install nose
+usr/local/bin/pip-3 install BeautifulSoup4
+usr/local/bin/pip-3 install bs4
+usr/local/bin/pip-3 install Flask-SQLAlchemy
 echo 'Running Flask Server' >> /var/log/sga-teamaurora-flask-install1.log
 #export FLASK_APP=StormDetection.py
 #flask run --host=0.0.0.0 --port=5000 >> /var/log/sga-teamaurora-StormDetection-server.log 2>&1 &
 
 echo 'starting Storm Detection'
 
-cd ../../
+cd ../
 /usr/local/bin/docker-compose up -d >> /var/log/SGATeamAuroraMileStone2/sga-teamaurora-StormDetector-serverM2.log
 #pwd >> /var/log/sga-teamaurora-StormDetection-server1.log
 #ls -la >> /var/log/sga-teamaurora-StormDetection-server1.log 
