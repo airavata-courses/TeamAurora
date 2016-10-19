f66884495052fbfcd07734ca6f5db743f3141c34
Srikanth <sriksrin@iu.edu>
2016-10-19 16:08:13 -0400

    added docker-compse.yml

--- StormClustering/scripts/before-install.sh
+++ StormClustering/scripts/before-install.sh
@@ -1,8 +1,7 @@
 #echo 'Kill All the Process' >> /var/log/flask-before.log
 #ps -ef | grep python | grep -v grep | awk '{print $2}' | xargs kill >> /var/log/flask-before.log
 #sleep 5
-#docker rm -f $(docker ps -a)
-
+docker rm pythonlibs_stormclustering_1
 pwd >> /var/log/SGATeamAuroraMileStone2/sga-teamaurora-StormClustering-beforeInstallM2.log
 cd /home/ec2-user/python_libs/stormclustering/StormClustering/
-docker build -t stormclustering .  >> /var/log/SGATeamAuroraMileStone2/sga-teamaurora-StormClustering-serverM2.log
+# docker build -t stormclustering .  >> /var/log/SGATeamAuroraMileStone2/sga-teamaurora-StormClustering-serverM2.log
