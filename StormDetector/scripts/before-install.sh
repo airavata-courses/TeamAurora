1bddc8178ec7c0835ecc133e635965c188e50a5e
srikanth holavanahalli <srikanth.holavanahalli@gmail.com>
2016-10-19 18:09:01 -0400

    added docker commands to install.sh

--- StormDetector/scripts/before-install.sh
+++ StormDetector/scripts/before-install.sh
@@ -2,4 +2,6 @@ echo 'Kill All the Process' >> /var/log/flask-before.log
 ps -ef | grep python | grep -v grep | awk '{print $2}' | xargs kill >> /var/log/flask-before.log
 sleep 5
 
+#docker rm pythonlibs_detector_1
+
 #chmod 777 /home/ec2-user/StormDetector/scripts/install.sh
--- StormDetector/scripts/install.sh
+++ StormDetector/scripts/install.sh
@@ -16,21 +16,24 @@ cd ./stormdetector
 
 echo 'Activating virtualenv for StormDetection Microservice' 
 #pip-3.4 install -r /home/ec2-user/python_libs/stormdetector/StormDetector/requirements.txt 
-pip-3.4 install virtualenv
+usr/local/bin/pip-3.4 install virtualenv
 #cd StormDetector
 virtualenv env
 source env/bin/activate
-pip-3.4 install Flask
-pip-3.4 install nose
-pip-3.4 install BeautifulSoup4
-pip-3.4 install bs4
-pip-3.4 install Flask-SQLAlchemy
+usr/local/bin/pip-3.4 install Flask
+usr/local/bin/pip-3.4 install nose
+usr/local/bin/pip-3.4 install BeautifulSoup4
+usr/local/bin/pip-3.4 install bs4
+usr/local/bin/pip-3.4 install Flask-SQLAlchemy
 echo 'Running Flask Server' >> /var/log/sga-teamaurora-flask-install1.log
 #export FLASK_APP=StormDetection.py
 #flask run --host=0.0.0.0 --port=5000 >> /var/log/sga-teamaurora-StormDetection-server.log 2>&1 &
 
 echo 'starting Storm Detection'
+
+cd ../../
+/usr/local/bin/docker-compose up -d >> /var/log/SGATeamAuroraMileStone2/sga-teamaurora-StormDetector-serverM2.log
 #pwd >> /var/log/sga-teamaurora-StormDetection-server1.log
 #ls -la >> /var/log/sga-teamaurora-StormDetection-server1.log 
-python3 ./StormDetector/stormDetector.py >> /var/log/sga-teamaurora-StormDetection-server1.log 2>&1 &
-python3 /home/ec2-user/python_libs/stormclustering/StormClustering/StormClustering.py >> /var/log/sga-teamaurora-StormClustering-server1.log 2>&1 &
+#python3 ./StormDetector/stormDetector.py >> /var/log/sga-teamaurora-StormDetection-server1.log 2>&1 &
+#python3 /home/ec2-user/python_libs/stormclustering/StormClustering/StormClustering.py >> /var/log/sga-teamaurora-StormClustering-server1.log 2>&1 &
