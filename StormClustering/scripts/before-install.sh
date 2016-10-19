5dc6d9eb7fca53cc656c1005ad512958f2d3c36b
SrikanthHolavanahalli <sriksrin@iu.edu>
2016-10-18 23:07:00 -0400

    updated path

--- StormClustering/scripts/before-install.sh
+++ StormClustering/scripts/before-install.sh
@@ -2,4 +2,7 @@
 #ps -ef | grep python | grep -v grep | awk '{print $2}' | xargs kill >> /var/log/flask-before.log
 #sleep 5
 #docker rm -f $(docker ps -a)
+
+pwd >> /var/log/SGATeamAuroraMileStone2/sga-teamaurora-StormClustering-beforeInstallM2.log
+cd /home/ec2-user/python_libs/stormclustering/StormClustering/
 docker build -t stormclustering .  >> /var/log/SGATeamAuroraMileStone2/sga-teamaurora-StormClustering-serverM2.log
