a24533a21f2314ad006c2d28bfa20d8ca0341811
SrikanthHolavanahalli <sriksrin@iu.edu>
2016-10-19 19:54:22 -0400

    updated before-install.sh with docker commands

--- StormDetector/scripts/before-install.sh
+++ StormDetector/scripts/before-install.sh
@@ -2,6 +2,6 @@ echo 'Kill All the Process' >> /var/log/flask-before.log
 ps -ef | grep python | grep -v grep | awk '{print $2}' | xargs kill >> /var/log/flask-before.log
 sleep 5
 
-/usr/local/bin/docker-compose rm -f pythonlibs_detector_1
+/usr/local/bin/docker rm -f pythonlibs_detector_1
 
 #chmod 777 /home/ec2-user/StormDetector/scripts/install.sh
