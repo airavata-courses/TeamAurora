dc519c7f2b4c60893b8378341c55c6e5e11319e5
pratiksanghvi <pratiksanghvi.ms@gmail.com>
2016-10-20 02:34:34 -0400

    Updated.

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,8 +7,8 @@ cd apigateway
 sudo mkdir -p $(date +%m%d%Y)
 cd $(date +%m%d%Y)
 
-cp /home/ec2-user/appspec.yml .
-cp -R /home/ec2-user/api_gateway/ .
+sudo cp /home/ec2-user/appspec.yml .
+sudo cp -R /home/ec2-user/api_gateway/ .
 sudo cp /home/ec2-user/api_gateway/apigateway.war ./api_gateway/docker/images/.
 
 cd /home/ec2-user/java_libs/apigateway/$(date +%m%d%Y)/api_gateway/docker/images/
