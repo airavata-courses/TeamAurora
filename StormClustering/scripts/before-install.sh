eeeca6225015d3d716308fbce85db5eea35a020f
srikanth holavanahalli <srikanth.holavanahalli@gmail.com>
2016-10-20 00:20:52 -0400

    reverted back docker compose

--- StormClustering/scripts/before-install.sh
+++ StormClustering/scripts/before-install.sh
@@ -1,7 +1,11 @@
 #echo 'Kill All the Process' >> /var/log/flask-before.log
 #ps -ef | grep python | grep -v grep | awk '{print $2}' | xargs kill >> /var/log/flask-before.log
 #sleep 5
-docker rm pythonlibs_stormclustering_1
+#docker rm -f ec2user_stormclustering_1
 pwd >> /var/log/SGATeamAuroraMileStone2/sga-teamaurora-StormClustering-beforeInstallM2.log
 cd /home/ec2-user/python_libs/stormclustering/StormClustering/
-# docker build -t stormclustering .  >> /var/log/SGATeamAuroraMileStone2/sga-teamaurora-StormClustering-serverM2.log
+
+docker stop stormclustering1
+docker rm -f stormclustering1
+
+docker build -t stormclustering .  >> /var/log/SGATeamAuroraMileStone2/sga-teamaurora-StormClustering-serverM2.log
