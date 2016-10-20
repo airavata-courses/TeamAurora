18e5052dab5616e1962755b8f665712dd677c48d
SrikanthHolavanahalli <sriksrin@iu.edu>
2016-10-20 01:47:29 -0400

    added docker rm command

--- StormDetector/scripts/before-install.sh
+++ StormDetector/scripts/before-install.sh
@@ -2,8 +2,8 @@ echo 'Kill All the Process' >> /var/log/flask-before.log
 ps -ef | grep python | grep -v grep | awk '{print $2}' | xargs kill >> /var/log/flask-before.log
 sleep 5
 
-#docker rm -f ec2user_stormdetector_1
+docker ps -a | grep 'stormdetector' | awk '{print $1}' | xargs --no-run-if-empty docker rm
 
 #chmod 777 /home/ec2-user/StormDetector/scripts/install.sh
-cd /home/ec2-user/python_libs/stormdetector/StormDtector/
+cd /home/ec2-user/python_libs/stormdetector/StormDetector/
 docker build -t stormdetector .
