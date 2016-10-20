0413feb0d1ec563301ad47cdd6344605a211d418
pratiksanghvi <pratiksanghvi.ms@gmail.com>
2016-10-20 01:00:58 -0400

    Updated import for GET request.

--- src/main/java/com/sg/aurora/apigateway/rest/resources/APIController.java
+++ src/main/java/com/sg/aurora/apigateway/rest/resources/APIController.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
