4e55b1669745ced28e327c292f0fc266d1747806
pratiksanghvi <pratiksanghvi.ms@gmail.com>
2016-10-20 19:19:02 -0400

    Updated files for docker related changes.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,20 @@
 echo 'Installing Forecast Trigger' 
-cp /home/ec2-user/forecast_trigger/forecasttrigger.war /usr/share/tomcat8/webapps/forecasttrigger.war
-echo 'Starting Tomcat service. This may take some time.'
-sudo service tomcat8 start
-sleep 30
+echo 'Copying artifacts to backup directory.'
+cd /home/ec2-user
+sudo mkdir -p java_libs
+cd java_libs
+sudo mkdir -p forecasttrigger
+cd forecasttrigger
+sudo mkdir -p $(date +%m%d%Y)
+cd $(date +%m%d%Y)
+
+sudo cp /home/ec2-user/appspec.yml .
+sudo cp -R /home/ec2-user/forecast_trigger/ .
+sudo cp /home/ec2-user/forecast_trigger/forecasttrigger.war ./forecast_trigger/docker/images/.
+
+cd /home/ec2-user/java_libs/forecasttrigger/$(date +%m%d%Y)/forecast_trigger/docker/images/
+
+echo '***Building docker image. This may take some time.***'
+docker build -t img_forecasttrigger .
+docker run -p 8082:8080 --name forecasttrigger -d img_forecasttrigger
+echo '***Docker container started.***'
