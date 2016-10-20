0b4556e55b64d5c62f41f30d6c2026dcf1605a6c
pratiksanghvi <pratiksanghvi.ms@gmail.com>
2016-10-20 01:54:15 -0400

    Added echo after docker run.

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,4 +16,4 @@ cd /home/ec2-user/java_libs/apigateway/$(date +%m%d%Y)/api_gateway/docker/images
 echo '***Building docker image. This may take some time.***'
 docker build -t img_apigateway .
 docker run -p 8081:8080 --name apigateway -d img_apigateway
-sleep 10
+echo '***Docker container started.***'
