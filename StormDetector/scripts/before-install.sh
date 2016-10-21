0dfac26cdf247f3a0aa2d95724eb99598354c1fc
SrikanthHolavanahalli <sriksrin@iu.edu>
2016-10-20 22:51:58 -0400

    removed build commands

--- StormDetector/scripts/before-install.sh
+++ StormDetector/scripts/before-install.sh
@@ -2,9 +2,9 @@ echo 'Kill All the Process' >> /var/log/flask-before.log
 ps -ef | grep python | grep -v grep | awk '{print $2}' | xargs kill >> /var/log/flask-before.log
 sleep 5
 
-docker ps -a | grep 'stormdetector' | awk '{print $1}' | xargs --no-run-if-empty docker stop
-docker ps -a | grep 'stormdetector' | awk '{print $1}' | xargs --no-run-if-empty docker rm
+#docker ps -a | grep 'stormdetector' | awk '{print $1}' | xargs --no-run-if-empty docker stop
+#docker ps -a | grep 'stormdetector' | awk '{print $1}' | xargs --no-run-if-empty docker rm
 
 #chmod 777 /home/ec2-user/StormDetector/scripts/install.sh
-cd /home/ec2-user/python_libs/stormdetector/StormDetector/
-docker build -t stormdetector .
+#cd /home/ec2-user/python_libs/stormdetector/StormDetector/
+#docker build -t stormdetector .
