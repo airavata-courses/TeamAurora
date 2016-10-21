a4851fe0203cc4539ffecb67c3d83326d770a5d3
SrikanthHolavanahalli <sriksrin@iu.edu>
2016-10-20 22:47:54 -0400

    removed docker rm commands

--- StormClustering/scripts/before-install.sh
+++ StormClustering/scripts/before-install.sh
@@ -3,7 +3,7 @@
 #sleep 5
 #docker rm -f ec2user_stormclustering_1
 pwd >> /var/log/SGATeamAuroraMileStone2/sga-teamaurora-StormClustering-beforeInstallM2.log
-cd /home/ec2-user/python_libs/stormclustering/StormClustering/
+#cd /home/ec2-user/python_libs/stormclustering/StormClustering/
 
 #docker ps -a | grep 'stormclustering' | awk '{print $1}' | xargs --no-run-if-empty docker stop
 #docker ps -a | grep 'stormclustering' | awk '{print $1}' | xargs --no-run-if-empty docker rm
