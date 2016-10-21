4118fc43d47891a7ce0917d52b8aba970bfdd740
SrikanthHolavanahalli <sriksrin@iu.edu>
2016-10-21 14:35:20 -0400

    removed log

--- StormClustering/scripts/install.sh
+++ StormClustering/scripts/install.sh
@@ -1,5 +1,4 @@
-echo 'starting installation process' >> /var/log/SGATeamAuroraMileStone2/sga-teamaurora-StormClustering1-install.log
-cd /home/ec2-user
+echo 'starting installation process' 
 mkdir -p python_libs
 cd python_libs
 mkdir -p stormclustering
@@ -11,11 +10,11 @@ cd stormclustering/StormClustering/
 docker ps -a | grep 'stormclustering' | awk '{print $1}' | xargs --no-run-if-empty docker stop
 docker ps -a | grep 'stormclustering' | awk '{print $1}' | xargs --no-run-if-empty docker rm
 
-docker build -t stormclustering .  >> /var/log/SGATeamAuroraMileStone2/sga-teamaurora-StormClustering-serverM2.log
+docker build -t stormclustering .
 
-echo "Starting Docker run" >> /var/log/SGATeamAuroraMileStone2/sga-teamaurora-StormClustering-installM2.log
+echo "Starting Docker run"
 
-docker run --name stormclustering1 -p 60622:60615 -d stormclustering &  >> /var/log/SGATeamAuroraMileStone2/sga-teamaurora-StormClustering-serverM2.log
+docker run --name stormclustering1 -p 60622:60615 -d stormclustering &
 #python3 StormClustering/StormClustering.py
 #/usr/local/bin/docker-compose up -d >> /var/log/SGATeamAuroraMileStone2/sga-teamaurora-StormClustering-serverM2.log
 
