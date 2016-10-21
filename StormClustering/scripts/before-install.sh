280cbc5d8d873bc48f0b8e2f99e54041b54c2516
SrikanthHolavanahalli <sriksrin@iu.edu>
2016-10-21 14:32:12 -0400

    removed all scripts

--- StormClustering/scripts/before-install.sh
+++ StormClustering/scripts/before-install.sh
@@ -2,7 +2,7 @@
 #ps -ef | grep python | grep -v grep | awk '{print $2}' | xargs kill >> /var/log/flask-before.log
 #sleep 5
 #docker rm -f ec2user_stormclustering_1
-pwd >> /var/log/SGATeamAuroraMileStone2/sga-teamaurora-StormClustering-beforeInstallM2.log
+#pwd >> /var/log/SGATeamAuroraMileStone2/sga-teamaurora-StormClustering-beforeInstallM2.log
 #cd /home/ec2-user/python_libs/stormclustering/StormClustering/
 
 #docker ps -a | grep 'stormclustering' | awk '{print $1}' | xargs --no-run-if-empty docker stop
