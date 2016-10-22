1761be4ce28d21859bb9b5865c150dc11150f157
pratiksanghvi <pratiksanghvi.ms@gmail.com>
2016-10-21 22:48:39 -0400

    Added code for load testing.

--- src/main/java/com/sg/aurora/dataingestor/rest/resources/URLCreation.java
+++ src/main/java/com/sg/aurora/dataingestor/rest/resources/URLCreation.java
@@ -81,6 +81,23 @@ public class URLCreation {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+		//Code added to add more work for load test.
+		int[] a=new int[200000];
+		long sum=0;
+		for(int i=0;i<200000;i++)
+		{
+			a[i]=i;
+			sum+=i;
+			sum-=i;
+
+		}
+
+		for(int i=0;i<200000;i++)
+		{
+			a[i]=10;
+		}
+		
 		return urlData;
 	}
 
