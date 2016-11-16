f5dff7f143bfe179327c784df7b5d04d3f020722
pratiksanghvi <pratiksanghvi.ms@gmail.com>
2016-11-16 17:25:08 -0500

    Updated deployment group and made sender message persistent.

--- src/main/java/com/sg/aurora/apigateway/rabbitmq/producer/MessageSender.java
+++ src/main/java/com/sg/aurora/apigateway/rabbitmq/producer/MessageSender.java
@@ -6,6 +6,7 @@ import java.util.concurrent.TimeoutException;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
 import com.sg.aurora.common.utils.SerializerDeserializer;
 import com.sg.aurora.common.utils.Service;
 
@@ -16,11 +17,14 @@ public class MessageSender {
 	    factory.setHost("35.161.35.175");
 	    Connection connection = factory.newConnection();
 	    Channel channel = connection.createChannel();
+	    //Method below handles load balancing amongst multiple containers of the same service.
+	    int prefetchCount = 1;
+	    channel.basicQos(prefetchCount);
 	    
 	    boolean durable = true;
 	    channel.queueDeclare(queueName, durable, false, false, null);
 	    //channel.basicPublish("", queueName, null, SerializerDeserializer.serialize(message));
-	    channel.basicPublish("", queueName, null, message.getBytes());
+	    channel.basicPublish("", queueName, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
     	System.out.println("APIGATEWAY :: Message Sent to " + toServiceName);
 	    
 	    channel.close();
