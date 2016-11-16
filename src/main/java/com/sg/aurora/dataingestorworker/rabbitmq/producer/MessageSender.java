e03546d47f501a34017de2c2ee9f8c3356428035
pratiksanghvi <pratiksanghvi.ms@gmail.com>
2016-11-16 17:49:52 -0500

    Updated deployment group and made RabbitMQ messages persistent.

--- src/main/java/com/sg/aurora/dataingestorworker/rabbitmq/producer/MessageSender.java
+++ src/main/java/com/sg/aurora/dataingestorworker/rabbitmq/producer/MessageSender.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
 import com.sg.aurora.common.utils.*;
 
 public class MessageSender {
@@ -16,13 +17,16 @@ public class MessageSender {
 	    factory.setHost("35.161.35.175");
 	    Connection connection = factory.newConnection();
 	    Channel channel = connection.createChannel();
+	    //Method below handles load balancing amongst multiple containers of the same service.
+	    int prefetchCount = 1;
+	    channel.basicQos(prefetchCount);
 	    
 	    boolean durable = true;
 	    channel.queueDeclare(Constants.APIGATEWAY_ALLSERVICES_QUEUE, durable, false, false, null);
 	    ObjectMapper objMapper=new ObjectMapper();
 	    String outgoingStr = objMapper.writeValueAsString(urlData);
 	    //channel.basicPublish("", APIGATEWAY_ALLSERVICES_QUEUE, null, SerializerDeserializer.serialize(outgoingStr));
-	    channel.basicPublish("", Constants.APIGATEWAY_ALLSERVICES_QUEUE, null, outgoingStr.getBytes());
+	    channel.basicPublish("", Constants.APIGATEWAY_ALLSERVICES_QUEUE, MessageProperties.PERSISTENT_TEXT_PLAIN, outgoingStr.getBytes());
 	    System.out.println(" Message Sent ");
 	    
 	    channel.close();
