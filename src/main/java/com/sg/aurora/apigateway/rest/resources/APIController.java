3226434984b123334b5f58d6f85c26d5c6648d5c
vikrant <vkaushal@indiana.edu>
2016-12-11 14:58:53 -0500

    Gif download code

--- src/main/java/com/sg/aurora/apigateway/rest/resources/APIController.java
+++ src/main/java/com/sg/aurora/apigateway/rest/resources/APIController.java
@@ -1,8 +1,12 @@
 package com.sg.aurora.apigateway.rest.resources;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.util.Date;
@@ -154,6 +158,7 @@ public class APIController {
 	} catch (Exception e) {
 		e.printStackTrace();
 	}
+	
 	ResponseBuilder rb = Response.seeOther(targetURIForRedirection);
 	rb.entity("hello world");
 	rb.type("text/html");
@@ -161,4 +166,31 @@ public class APIController {
 	return rb.build();
   }
   
+  
+
+  @GET
+  @Path("/output")
+  public Response displayGif( @Context HttpServletRequest request){
+	  URI targetURIForRedirection = null;
+	  String host = request.getParameter("host");
+	  String taskId = request.getParameter("taskId");
+	/*
+	  try {
+		URL website = new URL("http://52.53.179.0:1338/download/team-aurora-devel-job_aurora_243-0-4abb707d-5817-44c7-9ef8-dda9607b8d14/wrfoutput/Precip_total.gif");
+		ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+		FileOutputStream fos = new FileOutputStream("Precip_total.gif");
+		fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+		
+		targetURIForRedirection = new URI(request.getContextPath()+"/jsp/jobdetails.jsp");
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+	*/
+	ResponseBuilder rb = Response.seeOther(targetURIForRedirection);
+	//rb.entity("hello world");
+	rb.type("text/html");
+	  //Response.seeOther(targetURIForRedirection).build();
+	return rb.build();
+	
+  }
 }
